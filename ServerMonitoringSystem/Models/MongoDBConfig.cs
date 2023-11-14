2ca0d5c7e32f33f1af93b51a573ad1cd5e3f443f
Ibrahim <inobani28@gmail.com>
2023-11-14 11:16:58 +0200

    Adding the needed models

---
+++ ServerMonitoringSystem/Models/MongoDBConfig.cs
@@ -0,0 +1,8 @@
+namespace ServerMonitoringAndNotification.Models
+{
+    public class MongoDBConfig
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
