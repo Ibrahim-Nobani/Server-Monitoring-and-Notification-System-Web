bb1033c763f77a0d73831d70e81dded2a7bf271b
Ibrahim <inobani28@gmail.com>
2023-11-14 13:07:15 +0200

    Adjusting the main program

--- ServerMonitoringSystem/Program.cs
+++ ServerMonitoringSystem/Program.cs
@@ -9,25 +9,21 @@ using Microsoft.Extensions.Options;
 using ServerMonitoringAndNotification.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-// Register your dependencies
-builder.Logging.AddConsole(); // Add other logging providers if needed
+builder.Logging.AddConsole();
 
 builder.Services.AddSingleton<ServerStatisticsCollector>();
 builder.Services.AddSingleton<RabbitMQConfig>(builder.Configuration.GetSection("RabbitMQConfig").Get<RabbitMQConfig>());
-//builder.Configuration.GetSection("RabbitMQConfig").Get<RabbitMQConfig>();
-//var rabbitMQConfig = builder.Configuration.GetSection("RabbitMQConfig").Get<RabbitMQConfig>();
+
 
 builder.Services.AddSingleton<IMessageQueue, RabbitMQMessageQueue>();
 builder.Services.AddSingleton<IMessageReceiver, RabbitMQMessageReceiver>();
 
 builder.Services.AddSignalR();
 
-// Register AnomalyDetectionConfig
 builder.Services.AddSingleton<AnomalyDetectionConfig>(builder.Configuration.GetSection("AnomalyDetectionConfig").Get<AnomalyDetectionConfig>());
 builder.Services.AddSingleton<AnomalyDetector>();
 
-// Create and register the ServerStatisticsPublisher
-// Register MongoDB configuration
+
 builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDBConfig"));
 builder.Services.AddSingleton<IMongoDatabase>(provider =>
 {
@@ -36,7 +32,6 @@ builder.Services.AddSingleton<IMongoDatabase>(provider =>
     return client.GetDatabase(config.DatabaseName);
 });
 
-// Register the ServerStatisticsRepository
 builder.Services.AddSingleton<IRepository<ServerStatistics>, ServerStatisticsRepository>();
 
 builder.Services.AddSingleton<ServerStatisticsPublisher>();
@@ -44,20 +39,17 @@ builder.Services.AddSingleton<ServerStatisticsReceiver>();
 
 
 
-// Add your server statistics publisher as a hosted service
 builder.Services.AddHostedService<ServerStatisticsPublisherService>();
 builder.Services.AddHostedService<ServerStatisticsReceiverService>();
 
 
 var app = builder.Build();
 
-// Your existing endpoints
 app.MapGet("/", () => "Hello World!");
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
 logger.LogInformation("Application starting.");
 
-// SignalR endpoint
 app.MapHub<AlertHub>("/alerthub");
 
 app.Run();
