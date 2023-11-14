cd7367780b2c77c9f15f3f27600425f88c590227
Ibrahim <inobani28@gmail.com>
2023-11-14 11:26:39 +0200

    Adding a signalR event consumer side to receive messages

---
+++ EventConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+var signalREventConsumerService = new SignalREventConsumerService("http://localhost:5079/alerthub");
+
+Console.WriteLine("Press Enter to start the SignalR Event Consumer Service...");
+Console.ReadLine();
+
+await signalREventConsumerService.StartAsync();
+
+Console.WriteLine("Press Enter to stop the SignalR Event Consumer Service...");
+Console.ReadLine();
+
+await signalREventConsumerService.StopAsync();
